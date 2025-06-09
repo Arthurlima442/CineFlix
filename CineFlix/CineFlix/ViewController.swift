6c56da671f57c93ff65886f1ebf5471ed48affcb
Arthur Lima <arthurlima442@gmail.com>
2025-06-09 17:31:17 +0100

    config the first project

---
+++ CineFlix/CineFlix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CineFlix
+//
+//  Created by Arthur Lima on 09/06/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
