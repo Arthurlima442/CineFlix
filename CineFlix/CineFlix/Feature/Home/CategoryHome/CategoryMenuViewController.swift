fb65202c334d4f9be938afbb296de163f4d9079c
Arthur Lima <arthurlima442@gmail.com>
2025-06-23 20:26:25 +0100

    first attempt at button with modal

---
+++ CineFlix/CineFlix/Feature/Home/CategoryHome/CategoryMenuViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CategoryMenuViewController.swift
+//  CineFlix
+//
+//  Created by Arthur Lima on 23/06/2025.
+//
+
+import UIKit
+
+
+
+class CategoryMenuViewController: UIViewController {
+
+    
+
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            view.backgroundColor = .red
+            
+        }
+    
+    }
+       
+
+    
+
+    
+
+
---
+++ CineFlix/CineFlix/Feature/Home/Screen/HomeMovieScreen.swift
@@ -0,0 +1,124 @@
+//
+//  HomeScreen.swift
+//  CineFlix
+//
+//  Created by Arthur Lima on 09/06/2025.
+//
+
+import UIKit
+
+protocol HomeMovieScreenProtocol: AnyObject {
+    func tappedPresentCategoryMenu()
+}
+
+class HomeMovieScreen: UIView {
+    
+    weak var delegate: HomeMovieScreenProtocol?
+    
+    lazy var cineFlixLabel: UILabel = {
+        let text = UILabel()
+        text.translatesAutoresizingMaskIntoConstraints = false
+        text.text = "CineFlix"
+        text.font = UIFont.systemFont(ofSize: 45)
+        text.textColor = .red
+        text.textAlignment = .center
+        return text
+    }()
+    
+    lazy var searchTextField: UITextField = {
+        let search = UITextField()
+        search.translatesAutoresizingMaskIntoConstraints = false
+        search.borderStyle = .roundedRect
+        search.clipsToBounds = true
+        search.layer.cornerRadius = 4
+        search.layer.borderColor = UIColor.white.cgColor
+        search.layer.borderWidth = 1
+        search.backgroundColor = .black
+        search.textColor = .white
+        // Placeholder branco
+        search.attributedPlaceholder = NSAttributedString(
+            string: "search movie:",
+            attributes: [.foregroundColor: UIColor.lightGray]
+        )
+        return search
+    }()
+    
+    lazy var menuButton: UIButton = {
+       let button = UIButton(type: .system)
+       let image = UIImage(systemName: "line.3.horizontal") // ou uma imagem customizada
+       button.setImage(image, for: .normal)
+       button.tintColor = .white
+        button.backgroundColor = .black
+       button.layer.cornerRadius = 8
+       button.translatesAutoresizingMaskIntoConstraints = false
+       button.addTarget(self, action: #selector(tappedPresentCategoryMenu), for: .touchUpInside)
+       return button
+   }()
+
+    @objc func tappedPresentCategoryMenu() {
+        delegate?.tappedPresentCategoryMenu()
+    }
+    
+    lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.register(MovieCarouselTableViewCell
+            .self, forCellReuseIdentifier: MovieCarouselTableViewCell
+            .identifier)
+        tableView.backgroundColor = .black
+        tableView.separatorStyle = .none
+        return tableView
+    }()
+    
+    init() {
+        super.init(frame: .zero)
+        backgroundColor = .black
+        addElements()
+        configConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func addElements() {
+        addSubview(tableView)
+        addSubview(cineFlixLabel)
+        addSubview(searchTextField)
+        addSubview(menuButton)
+    }
+    
+    func configTableViewProtocols(delegate: UITableViewDelegate, dataSource: UITableViewDataSource) {
+        tableView.delegate = delegate
+        tableView.dataSource = dataSource
+    }
+    
+    func configConstraints() {
+        NSLayoutConstraint.activate([
+            cineFlixLabel.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            cineFlixLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
+            cineFlixLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20),
+            cineFlixLabel.heightAnchor.constraint(equalToConstant: 40),
+            
+            menuButton.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            menuButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10),
+            
+            searchTextField.topAnchor.constraint(equalTo: cineFlixLabel.bottomAnchor, constant: 25),
+            searchTextField.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10),
+            searchTextField.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10),
+            searchTextField.heightAnchor.constraint(equalToConstant: 40),
+            
+            tableView.topAnchor.constraint(equalTo: searchTextField.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor),
+        ])
+    }
+}
+
+//NSLayoutConstraint.activate([
+//    menuButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16),
+//    menuButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+//    menuButton.widthAnchor.constraint(equalToConstant: 120),
+//    menuButton.heightAnchor.constraint(equalToConstant: 40)
+//])
